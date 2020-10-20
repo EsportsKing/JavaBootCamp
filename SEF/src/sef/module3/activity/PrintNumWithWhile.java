23685a8edb63f2ef32a525b40004460c99b3a508
Esports King <Esports King@DESKTOP-I0V3U3C>
2020-10-20 11:39:47 +0300

    Nick Rozenberg Print odd/even numbers in a while loop exercise

--- SEF/src/sef/module3/activity/PrintNumWithWhile.java
+++ SEF/src/sef/module3/activity/PrintNumWithWhile.java
@@ -12,25 +12,17 @@ public class PrintNumWithWhile {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		
-		
-		// Print all even numbers less than 100
-//		int i = 0;
-//		while(i<100){
-//			System.out.println(i);
-//			i=i+2;			
-//		}
-//		
+	public static void main(String[] args) {	
+		int i = 0;
+		while(i < 100) {
+			System.out.println(i);
+			i = i + 2;			
+		}		
 		 
-		// write code to Print all odd numbers less than 100
-		int j = 100;
+		int j = 1;
 		while(j < 100) {
 			System.out.println(j);
-			j = j +2;
-			
+			j = j + 2;	
 		}
-
 	}
-
 }
