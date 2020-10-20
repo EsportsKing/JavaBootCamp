cb4fd1aa02f8b3dcea1797855cb1556640950981
Esports King <Esports King@DESKTOP-I0V3U3C>
2020-10-20 09:40:39 +0300

    Commit from Eclipse Day 1 activities: 	Module 2

--- SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -12,11 +12,14 @@ public class VariableAssignmentActivity {
 	/**
 	 * @param args
 	 */
+	public static int test = 2;
 	public static void main(String[] args) {
 		
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
 		// 3- Print updated value to the console
+		test = 4;
+		System.out.println(test);
 
 	}
 
