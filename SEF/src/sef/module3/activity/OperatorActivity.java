49ce6992d2f01c5f431169651981f7bcb10c2447
Esports King <Esports King@DESKTOP-I0V3U3C>
2020-10-20 11:04:40 +0300

    Nick Rozenberg

--- SEF/src/sef/module3/activity/OperatorActivity.java
+++ SEF/src/sef/module3/activity/OperatorActivity.java
@@ -14,19 +14,21 @@ public class OperatorActivity {
 		// Declare numbers to be operated
 		int i = 8;
 		int j = 5;
+		int result = 0;
 		
-		// Subtract numbers
-		int result = i-j;
-		
-		// Print result
-		System.out.println("Difference = " + result);
-		
-		// Add numbers
-		result = i+j;
-		
-		// Print result
-		System.out.println("Sum = " + result);
-		
+		if (i >= 8) {
+			// Subtract numbers
+			result = i-j;
+			
+			// Print result
+			System.out.println("Difference = " + result);
+		}
+		else {
+			// Add numbers
+			result = i+j;
+			
+			// Print result
+			System.out.println("Sum = " + result);
+		}		
 	}
-
 }
