8fb5fe53fb91db5fc6546378a541da63dd04da40
Esports King <Esports King@DESKTOP-I0V3U3C>
2020-10-22 09:33:46 +0300

    Nick Rozenberg
    Changing ricky from a rectangle to a shape

--- SEF/src/sef/module6/activity/AbstractionActivity.java
+++ SEF/src/sef/module6/activity/AbstractionActivity.java
@@ -5,7 +5,7 @@ public class AbstractionActivity {
 	public static void main(String[] args) {
 		
 		// Constructing Ricky the rectangle
-		Rectangle ricky = new Rectangle(5,6);
+		Shape ricky = new Rectangle(5,6);
 		
 		// Calculating Ricky's parameters
 		double Area = ricky.calculateArea();
